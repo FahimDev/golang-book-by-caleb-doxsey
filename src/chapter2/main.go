9067c801878ab37f843ad16e5495b4b7619906dd
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-03-01 13:06:45 +0600

    Escape Sequences and Comment

--- src/chapter2/main.go
+++ src/chapter2/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// escape sequences and comment
+
+/*
+Author: Md. Ariful Islam
+Date: 01/03/2023
+*/
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Md. Ariful Islam is working at \"Brain Station 23 Ltd.\"")
+	fmt.Println("His has got 3.26\\4 in BSc.")
+	fmt.Println("He have expertise in \n - Development and \n - Automation-QA \nboth side.")
+	fmt.Println("He have published research \nin \t Blockchain.")
+	fmt.Println("and \t IoT.")
 }
