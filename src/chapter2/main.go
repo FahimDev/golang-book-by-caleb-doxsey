ff0534a3dd045cb86137ed9ba29a67a0514e9cc1
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-03-01 14:18:20 +0600

    Variables and Formatting outputs
    Here we have used `%.2f` to print the float value in 2 decimal points.

--- src/chapter2/main.go
+++ src/chapter2/main.go
@@ -10,9 +10,16 @@ Date: 01/03/2023
 */
 
 func main() {
-	fmt.Println("Md. Ariful Islam is working at \"Brain Station 23 Ltd.\"")
-	fmt.Println("His has got 3.26\\4 in BSc.")
+
+	var testBool bool = true               // Boolean
+	var totalCGPA int = 4                  // Integer
+	var myCGPA float32 = 3.26              // Floating point number
+	var myName string = "Md. Ariful Islam" // String
+
+	fmt.Printf("%s is working at \"Brain Station 23 Ltd.\"", myName)
+	fmt.Printf("\nHis has got %.2f\\%d in BSc.\n", myCGPA, totalCGPA)
 	fmt.Println("He have expertise in \n - Development and \n - Automation-QA \nboth side.")
 	fmt.Println("He have published research \nin \t Blockchain.")
 	fmt.Println("and \t IoT.")
+	fmt.Println(testBool)
 }
