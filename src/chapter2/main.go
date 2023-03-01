e755c9b8ca679c52e9f6728552fc6fa224014c12
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-03-01 15:02:39 +0600

    Constant, advanced variable declaring & formatting output

--- src/chapter2/main.go
+++ src/chapter2/main.go
@@ -11,15 +11,17 @@ Date: 01/03/2023
 
 func main() {
 
-	var testBool bool = true               // Boolean
-	var totalCGPA int = 4                  // Integer
-	var myCGPA float32 = 3.26              // Floating point number
-	var myName string = "Md. Ariful Islam" // String
+	testBool := true             // Boolean
+	totalCGPA := 4               // Integer
+	myCGPA := 3.26               // Floating point number
+	myName := "Md. Ariful Islam" // String
+	const COUNTRY = "Bangladesh"
 
-	fmt.Printf("%s is working at \"Brain Station 23 Ltd.\"", myName)
-	fmt.Printf("\nHis has got %.2f\\%d in BSc.\n", myCGPA, totalCGPA)
+	fmt.Printf("%v is working at \"Brain Station 23 Ltd.\"", myName)
+	fmt.Printf("\nHe has got %v\\%v in BSc.\n", myCGPA, totalCGPA)
 	fmt.Println("He have expertise in \n - Development and \n - Automation-QA \nboth side.")
 	fmt.Println("He have published research \nin \t Blockchain.")
 	fmt.Println("and \t IoT.")
+	fmt.Printf("He is from %v \n", COUNTRY)
 	fmt.Println(testBool)
 }
