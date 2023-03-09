d49e8ac3dfbc0d62bc3a104e5eb1aa5c597751f5
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-03-09 16:38:21 +0600

    Control Structures
    - if/else
    - for
    - switch

---
+++ src/chapter5/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// control structures
+
+/*
+Author: Md. Ariful Islam
+Date: 01/03/2023
+*/
+
+func main() {
+	var inputNum int
+	fmt.Printf("Please, entry an integer number: ")
+	fmt.Scan(&inputNum)
+
+	switch inputNum {
+	case 0:
+		fmt.Println("Zero for switch trial")
+	case 1:
+		fmt.Println("Input 1 is engaged")
+	default:
+		fmt.Println("Executing switch default...")
+		if inputNum%2 != 0 {
+			i := 1
+			for i <= 10 {
+				fmt.Println(i)
+				i++
+			}
+		} else {
+			for i := 10; i >= 1; i-- {
+				fmt.Println(i)
+			}
+		}
+	}
+
+}
