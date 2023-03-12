0cd1fed7e5633adb021cd393deb943e1040d9e92
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-03-12 13:12:12 +0600

    `Closure` Function in two different approach

--- src/chapter7/main.go
+++ src/chapter7/main.go
@@ -26,8 +26,35 @@ func variadicFunctions(args ...int) int {
 	return total
 }
 
+func makeClosure() func() uint {
+	/* This method is returning a closure function which will return an uint value. */
+	i := uint(0) // Declaring a variable with value 0.
+	return func() (ret uint) {
+		/*  A closure is a function value that references variables from outside its body. */
+		ret = i // closure function setting return value as `i`.
+		i += 2  // after setting the return value closure function is updating the value.
+		return  // Defining the return syntax which will return the `rat` as non-updated `i` value.
+	}
+}
+
 func main() {
 	x, y := multiReturn()
 	fmt.Printf("Return value 1: %v and return value 2: %v \n", x, y)
 	fmt.Println("Total sum of parameters: ", variadicFunctions(1, 2, 3))
+
+	/* Closure Test 1 */
+	c := 0
+	increment := func() int {
+		c++
+		return c
+	}
+	fmt.Println("Closure Test1: ", increment())
+	fmt.Println("Closure Test1: ", increment())
+
+	/* Closure Test 2 */
+	// fmt.Println(makeClosure()) // Will print ==> 0x93b200 as closure function address
+	nextEven := makeClosure()
+	fmt.Println("Closure Test2: ", nextEven()) // 0
+	fmt.Println("Closure Test2: ", nextEven()) // 2
+	fmt.Println("Closure Test2: ", nextEven()) // 4
 }
